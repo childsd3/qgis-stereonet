ec83fb677b56360305e6e8e4d82cf54876935dc6
Daniel Childs <daniel@childsgeo.com>
2016-12-01 08:51:59 -0700

    alpha build

--- __init__.py
+++ __init__.py
@@ -22,14 +22,78 @@
  This script initializes the plugin, making it known to QGIS.
 """
 
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import sys
+import numpy as np
 
-# noinspection PyPep8Naming
-def classFactory(iface):  # pylint: disable=invalid-name
-    """Load Stereonet class from file Stereonet.
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import mplstereonet
+from qgis.core import *
+from qgis.gui import *
+import qgis.utils
 
-    :param iface: A QGIS interface instance.
-    :type iface: QgsInterface
-    """
-    #
-    from .stereonet import Stereonet
+import random
+
+def classFactory(iface):
     return Stereonet(iface)
+
+
+class Stereonet:
+    def __init__(self, iface):
+        self.iface = iface
+#        super(Stereonet, self).__init__()
+
+    def initGui(self):
+#        self.action = QAction(u'Go!', self.iface.mainWindow())
+#        self.action.triggered.connect(self.run)
+#        self.iface.addToolBarIcon(self.action)
+        self.contourAction = QAction(u'Contour', self.iface.mainWindow())
+        self.contourAction.triggered.connect(self.contourPlot)
+        self.iface.addToolBarIcon(self.contourAction)
+
+    def unload(self):
+#        self.iface.removeToolBarIcon(self.action)
+#        del self.action
+        self.iface.removeToolBarIcon(self.contourAction)
+        del self.contourAction
+
+    def run(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='stereonet')
+        strikes = list()
+        dips = list()
+        try:
+            layer = qgis.utils.iface.activeLayer()
+            iter = layer.selectedFeatures()
+            for feature in iter:
+                strikes.append(feature['ddr']-90)
+                dips.append(feature['dip'])
+            ax.pole(strikes, dips, 'k+', markersize=7)
+            ax.grid()
+            plt.show()
+        except:
+            return
+    def contourPlot(self):
+        fig, ax = mplstereonet.subplots()
+        strikes = list()
+        dips = list()
+        try:
+            layer = qgis.utils.iface.activeLayer()
+            print(layer)
+            iter = layer.selectedFeatures()
+            for feature in iter:
+                strikes.append(feature['ddr']-90)
+                dips.append(feature['dip'])
+            cax = ax.density_contourf(strikes, dips, measurement='poles',cmap=cm.coolwarm)
+            
+            ax.pole(strikes, dips, 'k+', markersize=7)
+            ax.grid(True)
+            fig.colorbar(cax)
+            
+            plt.show()
+        except:
+            return
