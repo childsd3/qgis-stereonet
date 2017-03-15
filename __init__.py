fb4e7a90993818fbf8fe0f88669e6e05ddae776f
Daniel Childs <dchilds@cnitucson.com>
2017-03-15 11:31:19 -0700

    added support for reading strikes

--- __init__.py
+++ __init__.py
@@ -5,19 +5,19 @@
                                  A QGIS plugin
  Displays a geologic stereonet of selected data
                              -------------------
-        begin                : 2016-11-29
-        copyright            : (C) 2016 by Daniel Childs
-        email                : daniel@childsgeo.com
-        git sha              : $Format:%H$
+        begin               : 2016-11-29
+        copyright           : (C) 2016 by Daniel Childs
+        email               : daniel@childsgeo.com
+        git sha             : $Format:%H$
  ***************************************************************************/
 
 /***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by   *
+ *   the Free Software Foundation; either version 2 of the License, or      *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
  ***************************************************************************/
  This script initializes the plugin, making it known to QGIS.
 """
@@ -46,9 +46,7 @@ class Stereonet:
         self.contourAction = QAction(QIcon(str(dir_path)+"/icon.png"), u'Stereonet', self.iface.mainWindow())
         self.contourAction.triggered.connect(self.contourPlot)
         self.iface.addToolBarIcon(self.contourAction)
-
-        
-        print(dir_path)
+        print(dir_path,'v1')
 
     def unload(self):
         self.iface.removeToolBarIcon(self.contourAction)
@@ -75,17 +73,25 @@ class Stereonet:
         fig, ax = mplstereonet.subplots()
         strikes = list()
         dips = list()
-        try:
-            layers = self.iface.legendInterface().layers()
-            for layer in layers:
+        layers = self.iface.legendInterface().layers()
+        for layer in layers:
+            if layer.type() == QgsMapLayer.VectorLayer:
                 iter = layer.selectedFeatures()
+                strikeExists = layer.fieldNameIndex('strike')
+                ddrExists = layer.fieldNameIndex('ddr')
+                dipExists = layer.fieldNameIndex('dip')
                 for feature in iter:
-                    strikes.append(feature['ddr']-90)
-                    dips.append(feature['dip'])
-            cax = ax.density_contourf(strikes, dips, measurement='poles',cmap=cm.coolwarm)
-            ax.pole(strikes, dips, 'k+', markersize=7)
-            ax.grid(True)
-            fig.colorbar(cax)
-            plt.show()
-        except:
-            return
+                    if strikeExists != -1 and dipExists != -1:
+                        print(feature['strike'],feature['dip'])
+                        strikes.append(feature['strike'])
+                        dips.append(feature['dip'])
+                    elif ddrExists != -1 and dipExists != -1:
+                        strikes.append(feature['ddr']-90)
+                        dips.append(feature['dip'])
+            else:
+                continue
+        cax = ax.density_contourf(strikes, dips, measurement='poles',cmap=cm.coolwarm)
+        ax.pole(strikes, dips, 'k+', markersize=7)
+        ax.grid(True)
+        fig.colorbar(cax)
+        plt.show()
